d77bbf418cd808df4a665daa2e3f9a15ed83f23d
Lilian <lilianbour0@gmail.com>
2020-02-28 14:36:22 +0100

    Adding writing on csv file and graph on first csv for Parcels_220_1176.0

--- Source.cpp
+++ Source.cpp
@@ -2,8 +2,9 @@
 #include "opencv2/core/core.hpp"  
 #include "opencv2/highgui/highgui.hpp"  
 #include <opencv2/imgcodecs.hpp>
-#include<iostream>
+#include <iostream>
 #include <filesystem>
+#include <fstream>
 
 namespace fs = std::filesystem;
 using namespace std;
@@ -14,17 +15,17 @@ int main()
 
 	//Get list of all files in folder
 	vector<string> liste_img;
-	std::string path = "C:/Users/lilia/Desktop/Parcels/220/1176.0";
+	std::string path = "C:/Users/lilia/Desktop/Parcels/220/1176.0";//IF CHANGING FOLDER NEED TO MODIFY EXCEL FILE NAME BELOW
 	for (const auto& entry : fs::directory_iterator(path)) { liste_img.push_back(entry.path().string()); }// liste_img.push_back(entry.path());
 
 
 
-	//vector(pair(pair(T1,T2),T3)) T1=counter / T2=motif / T3=list of images with this motif
-	vector<pair<pair<int, vector<int>>, vector<vector<int>>>> counter_motif_imgs;
+	//vector(pair(pair(T1,T2),T3)) T1=counter / T2=pattern / T3=list of images with this pattern
+	vector<pair<pair<int, vector<int>>, vector<vector<int>>>> counter_pattern_imgs;
 	//Init vector
 	vector<int> vec = { 0,0,0,0,0 };
 	vector<vector<int>> vec2 = { {0000,0000,0000},{0000,0000,0000} };
-	counter_motif_imgs.push_back(make_pair(make_pair(1, vec),vec2));
+	counter_pattern_imgs.push_back(make_pair(make_pair(1, vec),vec2));
 	//For each file in folder
 	for (size_t i = 1; i < liste_img.size()-1; i++)
 	{
@@ -73,44 +74,57 @@ int main()
 			}
 		}
 		bool boolean;
-		//Compare to all motifs
-		for (int i = 0; i < counter_motif_imgs.size(); i++) {
-			//If motif in motifs add +1 to counter
-			if (counter_motif_imgs[i].first.second == cube) {
-				counter_motif_imgs[i].first.first = counter_motif_imgs[i].first.first +1;
-				counter_motif_imgs[i].second.push_back({ date0,date1,date2 });
+		//Compare to all patterns
+		for (int i = 0; i < counter_pattern_imgs.size(); i++) {
+			//If pattern in patterns add +1 to counter
+			if (counter_pattern_imgs[i].first.second == cube) {
+				counter_pattern_imgs[i].first.first = counter_pattern_imgs[i].first.first +1;
+				counter_pattern_imgs[i].second.push_back({ date0,date1,date2 });
 				boolean = false;
 			}
-			//If motif not in motifs set true 
+			//If pattern not in patterns set true 
 			else {
 				boolean = true;
 			}
 		}
-		//if true add pair (1,motif) 
+		//if true add pair (1,pattern) 
 		if (boolean==true) {
 			vector<vector<int>> dates = { {date0,date1,date2} };
-			counter_motif_imgs.push_back(make_pair(make_pair(1, cube), dates));
+			counter_pattern_imgs.push_back(make_pair(make_pair(1, cube), dates));
 		}
 	}	
-	//Sorting and reversing counter_motif_imgs by highest counter of motif
-	sort(counter_motif_imgs.begin(), counter_motif_imgs.end());
-	reverse(counter_motif_imgs.begin(), counter_motif_imgs.end());
-	for (int j = 0; j < counter_motif_imgs.size(); j++) {
-		cout << "Motif " << j << " appearing " << counter_motif_imgs[j].first.first << " times"<< endl;
-		if (counter_motif_imgs[j].first.first > 1) {
-			cout << "Motif : \n";
-			for (int i = 0; i < counter_motif_imgs[j].first.second.size(); i++) {
-				cout << counter_motif_imgs[j].first.second[i] << " ";
-			}
-			cout << "\nImages : \n";
-			for (int i = 0; i < counter_motif_imgs[j].second.size(); i++) {
-				for (int k = 0; k < counter_motif_imgs[j].second[i].size(); k++) {
-					cout << counter_motif_imgs[j].second[i][k] << "  ";
-				}
-				cout << endl;
+	//Sorting and reversing counter_pattern_imgs by highest counter of pattern
+	sort(counter_pattern_imgs.begin(), counter_pattern_imgs.end());
+	reverse(counter_pattern_imgs.begin(), counter_pattern_imgs.end());
+	//Deletin last element 
+	counter_pattern_imgs.pop_back();
+
+	ofstream MyExcelFile;
+	MyExcelFile.open("C:/Users/lilia/github/Projet_ter/Parcels_220_1176.0.csv");//MODIFY HERE
+	MyExcelFile << "Counter;Images" << endl;
+	for (int j = 0; j < counter_pattern_imgs.size(); j++) {
+		cout << "Pattern " << j << " appearing " << counter_pattern_imgs[j].first.first << " times"<< endl;
+		MyExcelFile << counter_pattern_imgs[j].first.first << ";";
+		if (counter_pattern_imgs[j].first.first > 1) {
+			cout << "pattern : \n";
+			for (int i = 0; i < counter_pattern_imgs[j].first.second.size(); i++) {
+				cout << counter_pattern_imgs[j].first.second[i] << " ";
+				//MyExcelFile << counter_pattern_imgs[j].first.second[i];
 			}
 			cout << endl;
 		}
+		cout << "Images : \n";
+		for (int i = 0; i < counter_pattern_imgs[j].second.size(); i++) {
+			for (int k = 0; k < counter_pattern_imgs[j].second[i].size(); k++) {
+				cout << counter_pattern_imgs[j].second[i][k] << "  ";
+				MyExcelFile << counter_pattern_imgs[j].second[i][k] << ",";
+			}
+			cout << endl;
+			MyExcelFile << ";";
+		}
+		cout << endl;
+		MyExcelFile << endl;
 	}
+	MyExcelFile.close();
 	return 0;
 }
